30c85518ba09f75eea90acfa1f877c8d68cfef4b
Cathonia <68382607+Catfiji@users.noreply.github.com>
2021-02-11 17:39:15 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,42 @@
-#include <stdio.h>
 
+#include <stdio.h>
+#include "color.h"
 struct player
 {   int balance
 ;   int taxis
 ;   
 }; 
 
-    struct player pla = {100, 2}
-;   int m = 10
-;   
+    struct player pla = {100, 2} // Player
+;   int m = 10 // How much taxi's give
+;   void game(void) // THE GAME
+;   int getin(void) // Get input
+;
+
+int getin(void)// Get input
+{   unsigned char x // x?
+;   scanf("%c", &x) // Getchar is overrated
+;   getchar() // Fixes not being able to use scanf twice
+;   return x
+;
+}
+
+void game(void)
+{   int profit = m * pla.taxis // profit
+;   printf(GRN " Balance: $%d\n", pla.balance) 
+;   printf(GRN " Taxis: %d\n", pla.taxis)  
+;   printf(GRN " Current Profit: %d\n", profit)
+;   // menu
+;   printf(RED " [1] End day\n [2] Upgrade Taxi's\n [3] Buy Taxi\n")
+;   if (getin() == 49)  // 49 = "1"
+{       pla.balance = pla.balance + profit // Add money to player balance
+;       game()
+;
+}
+};
 
 int main(void) 
-{   int profit = m * pla.taxis
-;   printf("Balance: %d\n", pla.balance) 
-;   printf("Taxis: %d\n", pla.taxis)  
-;   printf("Current Profit: %d\n", profit)
+{   game()
 ;   return 0
 ;
 }
